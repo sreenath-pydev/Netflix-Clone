ea0f112058a957258232de9405d0940a468b4997
Sreenath k k <sreenathkk00@gmail.com>
2024-10-20 23:40:51 +0530

    Create Footer component
    - Added Footer component with links to FAQ, Help Center, and more.
    - Included copyright information and educational purpose disclaimer.
    - Styled the footer for better layout and accessibility.

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <a href="#">FAQ</a>
+        <a href="#">Help Center</a>
+        <a href="#">Account</a>
+        <a href="#">Media Center</a>
+        <a href="#">Investor Relations</a>
+        <a href="#">Jobs</a>
+        <a href="#">Redeem Gift Cards</a>
+        <a href="#">Buy Gift Cards</a>
+        <a href="#">Ways to Watch</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Privacy</a>
+        <a href="#">Cookie Preferences</a>
+        <a href="#">Corporate Information</a>
+        <a href="#">Contact Us</a>
+      </div>
+      <div className="footer-info">
+        <p>© 2024 Netflix Clone, All Rights Reserved.</p>
+        <p>Netflix clone created for educational purposes only.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
