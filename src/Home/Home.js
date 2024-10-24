4e54cf146b55e1aff49b54205a5ef1442c776787
Sreenath k k <sreenathkk00@gmail.com>
2024-10-24 14:44:09 +0530

    Enhanced Home component to include additional movie categories
    - Added new movie categories: NETFLIX ORIGINALS, ACTIONS, HORROR, ROMANCE, DOCUMENTARY, and TRENDING.
    - Imported the corresponding URLs from the `Urls.js` file to fetch data for the new categories.
    - Updated layout to provide a more diverse selection of content on the homepage.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,15 +2,19 @@ import React from 'react'
 import Banner from "../Components/Banner/Banner";
 import NavBar from "../Components/Navbar/NavBar";
 import Rowpost from "../Components/RowPost/Rowpost";
-import {Actions, Horror} from '../Components/Urls.js'
+import {Actions, Horror, Originals, Romance, Documentary, Trending} from '../Components/Urls.js'
 import Footer from "../Components/Footer/Footer.js";
 export default function Home() {
   return (
     <div>
        <NavBar/>
       <Banner/>
-      <Rowpost url={Actions} title="Trending Now" isSmall/>
-      <Rowpost url={Horror} title="Horror" isSmall/>
+      <Rowpost url={Originals} title="NETFLIX ORIGINALS" isSmall/>
+      <Rowpost url={Actions} title="ACTIONS" isSmall/>
+      <Rowpost url={Horror} title="HORROR" isSmall/>
+      <Rowpost url={Romance} title="ROMANCE" isSmall/>
+      <Rowpost url={Documentary} title="DOCUMENTARY" isSmall/>
+      <Rowpost url={Trending} title="TRENDING" isSmall/>
       <Footer/>
     </div>
   )
