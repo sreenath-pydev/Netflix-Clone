502ae8080696e8fa7614ffec34115dea13436c20
Sreenath k k <sreenathkk00@gmail.com>
2024-10-24 14:38:24 +0530

    Add Firebase Auth state listener and routing updates
    - Integrated Firebase `onAuthStateChanged` to monitor user authentication state.
    - Redirect users to the Home page if authenticated, and to the Login page if not authenticated.
    - Added `useNavigate` hook to handle programmatic navigation based on user auth state.
    - Included `react-toastify` for notifications, with a `ToastContainer` component for displaying toasts throughout the app.
    - Applied dark theme to `ToastContainer`.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Home from "./Home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Login from "./Login/Login";
 import Player from "./Components/PLayer/Player";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase/Firebase";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect (() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        navigate("/");
+        console.log('logged in')
+    }else{
+        navigate("/login");
+        console.log('Logged out')
+    }
+    });
+  }, []);
+
   return (
     <div className="App">
+      <ToastContainer theme="dark" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
