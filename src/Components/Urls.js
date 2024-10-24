bfd56c4a9911dd731e1f2628b1f7cde62d580123
Sreenath k k <sreenathkk00@gmail.com>
2024-10-24 14:41:28 +0530

    Added new API links for movie genres in urls.js
    - Added API endpoint for Comedy movies using genre ID 35.
    - Added API endpoint for Romance movies using genre ID 10749.
    - Added API endpoint for Documentary movies using genre ID 99.
    - Included Trending movies API endpoint for weekly trends with language set to English (en-US).

--- src/Components/Urls.js
+++ src/Components/Urls.js
@@ -3,3 +3,7 @@ import { API_KEY } from "./Constants/Constants";
 export const Originals = `discover/tv?api_key=${API_KEY}&with_networks=213`;
 export const Actions = `discover/movie?api_key=${API_KEY}&with_genres=28`;
 export const Horror = `discover/movie?api_key=${API_KEY}&with_genres=27`;
+export const Comedy = `discover/movie?api_key=${API_KEY}&with_genres=35`;
+export const Romance = `discover/movie?api_key=${API_KEY}&with_genres=10749`;
+export const Documentary = `discover/movie?api_key=${API_KEY}&with_genres=99`;
+export const Trending = `trending/all/week?api_key=${API_KEY}&language=en-US`;
