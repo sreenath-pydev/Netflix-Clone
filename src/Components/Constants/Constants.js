e7da9e2b0104869c7cfd862138dcb20f15834383
sreenath k k <133633684+sreenath-pydev@users.noreply.github.com>
2025-04-04 16:37:23 +0530

    Update Constants.js

--- src/Components/Constants/Constants.js
+++ src/Components/Constants/Constants.js
@@ -1,3 +1,3 @@
 export const baceUrl = "https://api.themoviedb.org/3";
-export const API_KEY = "d8c7097e768f506ae9c7f434135d6635"; 
+export const API_KEY = "e9a47ed49b5769bd8116eab02183854a"; 
 export const imageUrl = "https://image.tmdb.org/t/p/original"
